0bf7dd535b97e89dd62d56237a2d7c7686d5361f
Darwin <charmful0x@gmail.com>
2023-11-16 18:31:13 +0100

    perf: a clear `help` command

--- cli/bin/cli.js
+++ cli/bin/cli.js
@@ -6,55 +6,72 @@ import { initer } from "./initer.js";
 import { handleDeployFunction } from "./handlers/deploy.js";
 import { handleWriteFunction } from "./handlers/write.js";
 
-const argvs = yargs(hideBin(process.argv))
-  .command({
+const commands = [
+  {
     command: "deploy [src] [init-state] [testnet]",
-    builder: (yargs) => {
-      yargs.options({
-        src: {
-          describe: "path to the function source code",
-          demandOption: true,
-        },
-        "init-state": {
-          describe: "A stringified initial state for the function",
-          demandOption: true,
-        },
-        "testnet": {
-          describe: "optional to deploy the function on MEM Carbon testnet",
-          demandOption: false,
-        },
-      });
+    description: "Deploys a function",
+    options: {
+      src: {
+        describe: "path to the function source code",
+        demandOption: true,
+      },
+      "init-state": {
+        describe: "A stringified initial state for the function",
+        demandOption: true,
+      },
+      testnet: {
+        describe: "optional to deploy the function on MEM Carbon testnet",
+        demandOption: false,
+      },
     },
-    handler: async (argv) => {
-      await handleDeployFunction(argv);
-    },
-  })
-  .command({
+    handler: handleDeployFunction,
+  },
+  {
     command: "write [function-id] [inputs] [testnet]",
-    builder: (yargs) => {
-      yargs.options({
-        "function-id": {
-          describe: "your MEM function ID",
-          demandOption: true,
-        },
-        "inputs": {
-          describe: "the function interaction stringified inputs object",
-          demandOption: true,
-        },
-        "testnet": {
-          describe: "optional to write the interaction on MEM Carbon testnet",
-          demandOption: false,
-        },
-      });
+    description: "Writes an interaction",
+    options: {
+      "function-id": {
+        describe: "your MEM function ID",
+        demandOption: true,
+      },
+      inputs: {
+        describe: "the function interaction stringified inputs object",
+        demandOption: true,
+      },
+      testnet: {
+        describe: "optional to write the interaction on MEM Carbon testnet",
+        demandOption: false,
+      },
     },
-    handler: async (argv) => {
-      await handleWriteFunction(argv);
-    },
-  })
-  .command({
+    handler: handleWriteFunction,
+  },
+  {
     command: "*",
-    handler: async (argv) => {
-      await initer();
+    description: "Initialize",
+    handler: initer,
+  },
+];
+
+const argvs = yargs(hideBin(process.argv))
+  .scriptName("mem")
+  .usage("Usage: $0 <command> [options]");
+
+commands.forEach(({ command, description, options, handler }) => {
+  argvs.command(
+    command,
+    description,
+    (yargs) => {
+      if (options) {
+        yargs.options(options);
+      }
     },
-  })
+    handler,
+  );
+});
+
+argvs
+  .demandCommand(1, "Please provide a valid command")
+  .epilogue("For more information, visit: https://docs.mem.tech")
+  .example("$0 deploy --src path/to/source --init-state initialState", "")
+  .alias("h", "help")
   .help().argv;
